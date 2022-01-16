69ddb6b0f774e198047e52b467bfbb454065502f
lfchaim <lfchaim@gmail.com>
2022-01-16 12:01:29 -0300

    Spring boot init

---
+++ src/main/java/com/whs/jwt/WhsJwtApplication.java
@@ -0,0 +1,13 @@
+package com.whs.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhsJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhsJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whs/jwt/WhsJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whs.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhsJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

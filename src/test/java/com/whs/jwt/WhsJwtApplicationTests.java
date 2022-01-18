9704c9e63ff430610456a2f662d1391f0de19af8
lfchaim <lfchaim@gmail.com>
2022-01-18 09:38:09 -0300

    MVP developed

--- src/test/java/com/whs/jwt/WhsJwtApplicationTests.java
+++ src/test/java/com/whs/jwt/WhsJwtApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WhsJwtApplicationTests {
+public class WhsJwtApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
